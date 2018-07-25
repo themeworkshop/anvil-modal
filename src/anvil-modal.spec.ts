0360952d69da9956393b50e542960bd9a406d49a
Craig Morrison <hello@craigmorrison.co.uk>
2018-07-26 00:42:58 +0100

    Allow modal to be opened and closed

--- src/anvil-modal.spec.ts
+++ src/anvil-modal.spec.ts
@@ -4,13 +4,14 @@ import AnvilModal from './anvil-modal';
 const html = `
 <body>
   <div data-component="modal">
-    <button id="modal-button" data-modal="button" aria-controls="modal-content">Open</button>
+    <button id="modal-button" data-modal="open-button" aria-controls="modal-content">Open</button>
     <section id="modal-content" hidden data-modal="content" aria-labelledby="modal-title">
       <h1 id="modal-title">This is a modal window</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
         ipsa ducimus a molestias cum, harum veritatis ea illum debitis
         aliquid obcaecati eligendi voluptates distinctio ratione delectus,
         iste voluptate, eos odit.</p>
+        <button data-modal="close-button" data-controls="modal-content">Close</button>
     </section>
   </div>
 </body>
@@ -26,14 +27,68 @@ describe('AnvilModal', () => {
     expect(anvil).toHaveProperty('components');
   });
 
-  it('opens when the button is clicked', () => {
+  it('opens when the open button is clicked', () => {
     const anvil = new Anvil();
     anvil.register('modal', AnvilModal);
 
-    const button: HTMLButtonElement = document.querySelector(
-      '[data-modal="button"]'
+    const openButton: HTMLButtonElement = document.querySelector(
+      '[data-modal="open-button"]'
     );
-    button.click();
-    expect(button.classList.contains('open-test')).toBe(true);
+    openButton.click();
+
+    const content: HTMLElement = document.querySelector(
+      '[data-modal="content"]'
+    );
+
+    const overlay: HTMLElement = document.querySelector(
+      '[data-modal="content"]'
+    );
+
+    expect(content.hidden).toBe(false);
+    expect(overlay.hidden).toBe(false);
+  });
+
+  it('closes when the close button is clicked', () => {
+    const anvil = new Anvil();
+    anvil.register('modal', AnvilModal);
+
+    const openButton: HTMLButtonElement = document.querySelector(
+      '[data-modal="open-button"]'
+    );
+    openButton.click();
+
+    const closeButton: HTMLButtonElement = document.querySelector(
+      '[data-modal="close-button"]'
+    );
+    closeButton.click();
+
+    const overlay: HTMLElement = document.querySelector(
+      '[data-modal="overlay"]'
+    );
+
+    expect(overlay.hidden).toBe(true);
+  });
+
+  it('opens when it has been closed and is re-opened', () => {
+    const anvil = new Anvil();
+    anvil.register('modal', AnvilModal);
+
+    const openButton: HTMLButtonElement = document.querySelector(
+      '[data-modal="open-button"]'
+    );
+    openButton.click();
+
+    const closeButton: HTMLButtonElement = document.querySelector(
+      '[data-modal="close-button"]'
+    );
+
+    const overlay: HTMLElement = document.querySelector(
+      '[data-modal="overlay"]'
+    );
+
+    closeButton.click();
+    openButton.click();
+
+    expect(overlay.hidden).toBe(false);
   });
 });
--- src/anvil-modal.ts
+++ src/anvil-modal.ts
@@ -1,23 +1,47 @@
 class AnvilModal implements AnvilModal {
   id: number;
   root: Element;
-  button: Element;
-  content: Element;
+  openButton: HTMLButtonElement;
+  closeButton: HTMLButtonElement;
+  content: HTMLElement;
+  modalCreated: boolean;
+  overlay: HTMLElement;
 
   constructor(index: number, element: Element) {
     this.root = element;
     this.id = index;
-    this.button = this.root.querySelector('[data-modal="button"]');
+    this.openButton = this.root.querySelector('[data-modal="open-button"]');
+    this.closeButton = this.root.querySelector('[data-modal="close-button"]');
     this.content = this.root.querySelector('[data-modal="content"]');
+    this.modalCreated = false;
     this.load();
   }
 
   load() {
-    this.button.addEventListener('click', () => this.open());
+    this.openButton.addEventListener('click', () => this.openModal());
+    this.closeButton.addEventListener('click', () => this.closeModal());
   }
 
-  open() {
-    this.button.classList.add('open-test');
+  createModal() {
+    this.overlay = document.createElement('div');
+    this.overlay.setAttribute('id', `modal-overlay-${this.id}`);
+    this.overlay.setAttribute('data-modal', 'overlay');
+    document.body.appendChild(this.overlay);
+    document.body.appendChild(this.content);
+    this.content.hidden = false;
+    this.modalCreated = true;
+  }
+
+  openModal() {
+    if (!this.modalCreated) {
+      this.createModal();
+    } else {
+      this.overlay.hidden = false;
+    }
+  }
+
+  closeModal() {
+    this.overlay.hidden = true;
   }
 }
 
